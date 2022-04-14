d9fe1de3c1a8f9e5f2bbc118e68b880a1f00a3fc
goraider <alejandro_gosain@hotmail.com>
2022-04-14 03:18:03 -0500

    al agregar cada cuidad se podra visualizar la hora que es a partir de la que se selecciona

--- src/components/time-zone/TimeZoneCustom.js
+++ src/components/time-zone/TimeZoneCustom.js
@@ -1,27 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import CrudService from '../../services/api.service';
 import moment from 'moment-timezone';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { ScrollMenu } from 'react-horizontal-scrolling-menu';
 import './TimeZoneStyle.css';
 
 export const TimeZoneCustom = () => {
 
     const [ countries, setCountries ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
     const [ text, setText ] = useState('');
     const [ timeZone, setTimeZone ] = useState([]);
     const [ coincidences, setCoincidences ] = useState([]);
-    
+
     const [ hours, setHours ] = useState([]);
 
-    useEffect( () =>{
-        const loadCountries = async () => {
-            const response =  await CrudService.getAllCountries();
-            setCountries(response.data);
 
+
+    useEffect(() => {
+        try {
+
+            setLoading(true);
+
+            const loadCountries = async () => {
+                const response =  await CrudService.getAllCountries();
+                setCountries(response.data);
+
+            }
+            loadCountries();
+
+            setLoading(false);
+        } catch (error) {
+
+            setLoading(false);
+            console.log(error);
         }
-        loadCountries();
     },[])
-
 
     const onChangeHandler = (text) => {
         let matches = [];
@@ -59,20 +73,17 @@ export const TimeZoneCustom = () => {
             console.log("xxxx", timeZone);
         }
         loadTimeZoneCountrie();
-        
+
     }
 
-    const removeCountryListHandler = (i) => {
+    const removeCountryListHandler = (items,i) => {
 
         let country = [...timeZone];
-        console.log(country);
 
-        
         if (i !== -1) {
           country.splice(i, 1);
           setTimeZone(country);
         }
-
     }
 
     const timeZoneFormatHandler = (datetime, timezone) => {
@@ -84,7 +95,7 @@ export const TimeZoneCustom = () => {
     };
 
     const dateZoneFormatHandler = (datetime, timezone) => {
-        
+
         let date = moment(datetime).tz(timezone).format("ddd, MMMM Do YYYY");
 
         return date;
@@ -92,169 +103,146 @@ export const TimeZoneCustom = () => {
 
     const printHoursTimeZoneHandler = (obj) => {
 
-        let hours = [];
+        let hours_country = [];
+        let arr_hours = {};
 
         for (let hour = 1; hour < 35; hour++) {
 
-            let object_hours = {
-                hour: moment(obj.datetime).add(hour, 'h').tz(obj.timezone).format("h a")
-            };
+            arr_hours = {
 
-            hours.push(  object_hours );
+                abbreviation : obj.abbreviation,
+                time_zone : obj.timezone,
+                hour : moment(obj.datetime).add(hour, 'h').tz(obj.timezone).format("h a"),
+                date : moment(obj.datetime).add(hour, 'h').tz(obj.timezone).format("ddd, MMMM Do YYYY")
+
+            }
+
+            hours_country.push( arr_hours );
+
+
         }
 
-        console.log("Hrs. ",hours);
-        setHours(hours);
+        let con = hours_country.concat(hours);
+        let update_country = con.filter((item, pos) => con.indexOf(item) === pos);
+
+        setHours([
+            ...update_country
+        ]);
 
     }
-
-    const LeftArrow = () => {
-        const { isFirstItemVisible, scrollPrev } =
-          React.useContext(VisibilityContext);
-      
-        return (
-          <div disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
-            <i class="fa fa-arrow-left" aria-hidden="true"></i>
-          </div>
-        );
-      }
-      
-      const RightArrow = () => {
-        const { isLastItemVisible, scrollNext } = React.useContext(VisibilityContext);
-      
-        return (
-          <div disabled={isLastItemVisible} onClick={() => scrollNext()}>
-            <i class="fa fa-arrow-right" aria-hidden="true"></i>
-          </div>
-        );
-      }
-      
 
 
 
     return (
-
         <React.StrictMode>
+            <div>
+                <h1>WorldtimeLite</h1>
+                <hr />
 
-        <div>
-            <h1>WorldtimeLite</h1>
-            <hr />
-            <div className="row">
-                <div className="col-4">
-                    <input
-                        type="text" className="col-4 form-control form-rounded input-search"
-                        placeholder="Find country - Press keyboard letter..."
-                        onChange={ e => onChangeHandler(e.target.value) }
-                        onKeyPress={ inputToUppercase }
-                        value={ text }
-                        onBlur={()=>{
-                            setTimeout(() => {
-                                setCoincidences([])
-                            }, 100);
-                        }}
-                    />
-                </div>
+                {
 
-            </div>
+                    loading
+                    ?
+                    (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    )
+                    :
+                    (
 
-
-            { coincidences && coincidences.map((coincidences, i) =>
-                <div key={i} className='coincidence border-0'
-                onClick={ () => onCoincidenceHandler(coincidences) }>
-                    { coincidences }
-                </div>
-            )}
-
-            <hr />
-
-            {
-                timeZone && timeZone.map( (items, i) =>(
-
-                        <div key={i} className='card card-stile' >
-
-                                <div className="card-body">
-                                    <div className="row">
-                                        <div className="col-1">
-
-                                            <button type="button" className="btn btn-light"
-
-                                                onClick={() => removeCountryListHandler(i) }
-                                            >
-                                                <i className="fa-solid fa-trash"></i>
-                                            </button>
-
-                                        </div>
-                                        <div className="col">
-                                            <h5 className="card-title">{ items.timezone }</h5>
-                                            <h6 className='card-subtitle text-muted'> {  timeZoneFormatHandler(items.datetime, items.timezone) } { items.abbreviation }</h6>
-                                            <p className='card-text'>
-                                               { dateZoneFormatHandler(items.datetime, items.timezone) } 
-                                            </p>
-                                        </div>
-
-                                        <div className="col-8">
-
-                                            {/* <div className="d-flex flex-row bd-highlight mb-3">
-                                                {
-
-                                                    hours && hours.map( (hour, j) =>(
-
-                                                        
-                                                            <div key={j} className="p-2 bd-highlight">
-                                                                    {hour.hour}
-                                                            </div>
-
-                                                    ))
-
-                                                }
-                                            </div> */}
-
-                                            {/* <ScrollMenu
-                                                LeftArrow={LeftArrow} RightArrow={RightArrow}
-                                                data={ hours.map( (hour, j) =>(
-                                                    <div key={j} className="p-2 bd-highlight">
-                                                        {hour.hour}
-                                                    </div>
-                                                ))}
-                                            /> */}
-
-                                            <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-                                                {hours.map( (hour, j) =>(
-                                                    <div key={j} className="p-2 bd-highlight box-hour">
-                                                        <strong className='element-box'>{hour.hour}</strong>
-                                                    </div>
-                                                ))}
-                                            </ScrollMenu>
-
-                                        </div>
-
-
-
-
-
-                                    </div>
-
-                                </div>
+                        <div className="row">
+                            <div className="col-4">
+                                <input
+                                    type="text" className="col-4 form-control form-rounded input-search"
+                                    placeholder="Find country - Press keyboard letter and Select to Country..."
+                                    onChange={ e => onChangeHandler(e.target.value) }
+                                    onKeyPress={ inputToUppercase }
+                                    value={ text }
+                                    onBlur={()=>{
+                                        setTimeout(() => {
+                                            setCoincidences([])
+                                        }, 100);
+                                    }}
+                                />
+                            </div>
                         </div>
 
-                        
+                    )
+                }
+
+                {
+                    coincidences && coincidences.map((coincidences, i) =>
+                        <div key={i} className='coincidence border-0'
+                            onClick={ () => onCoincidenceHandler(coincidences) }>
+                            { coincidences }
+                        </div>
+                    )
+                }
+
+                <hr />
+
+                {
+                    timeZone && timeZone.map( (items, i) =>(
+
+                            <div key={i} className='card card-stile' >
+
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-1">
+
+                                                <button type="button" className="btn btn-light"
+
+                                                    onClick={() => removeCountryListHandler(items,i) }
+                                                >
+                                                    <i className="fa-solid fa-trash"></i>
+                                                </button>
+
+                                            </div>
+                                            <div className="col">
+                                                <h5 className="card-title">{ items.timezone }</h5>
+                                                <h6 className='card-subtitle text-muted'> {  timeZoneFormatHandler(items.datetime, items.timezone) } { items.abbreviation }</h6>
+                                                <p className='card-text'>
+                                                { dateZoneFormatHandler(items.datetime, items.timezone) }
+                                                </p>
+                                            </div>
+
+                                            {
+                                                
+                                                <div className="col-8">
+                                                    <ScrollMenu 
+                                                        key={i}
+                                                    >
+
+                                                    {
+                                                    hours.filter(elements => (elements.time_zone === timeZone[i].timezone)).map((filteredTimeZone, j) => (
+
+                                                        <div key={j} className="p-2 bd-highlight element-box time-zone-country">
+                                                            <div className="card-body">
+                                                                <h5 className="card-title">{ filteredTimeZone.hour }</h5>
+                                                                <p className="card-text">{ filteredTimeZone.date }</p>
+                                                            </div>
+                                                        </div>
+
+                                                        ))
+                                                    }
 
 
+                                                    </ScrollMenu>
 
-                    // console.log('test', items);
-                    // return <li key={i}>{items.timezone}</li>
-                ))
-            }
+                                                </div>
+                                            }
 
+                                        </div>
 
+                                    </div>
+                            </div>
+                    ))
+                }
 
-            {/* { JSON.stringify(timeZone) } */}
+                {/* { JSON.stringify(timeZone) } */}
 
-
-
-
-
-        </div>
+            </div>
         </React.StrictMode>
     )
 }
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -8,21 +8,6 @@ class CrudService {
   getTimeZoneCountrie(countrie) {
     return http.get(`/timezone/${countrie}`);
   }
-  create(data) {
-    return http.post("/tutorials", data);
-  }
-  update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
-  }
-  delete(id) {
-    return http.delete(`/tutorials/${id}`);
-  }
-  deleteAll() {
-    return http.delete(`/tutorials`);
-  }
-  findByTitle(title) {
-    return http.get(`/tutorials?title=${title}`);
-  }
 }
 
 export default new CrudService();
